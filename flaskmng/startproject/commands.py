a9111fb1fbed97b336df458a791da07d59d33b90
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-04-04 16:31:46 +0400

    Command line redesigned

--- flaskmng/startproject/commands.py
+++ flaskmng/startproject/commands.py
@@ -1,23 +1,32 @@
 import os
 import click
-from ..utils import MultiCommand, command_process_step
+from ..utils import MultiCommand, command_process_step, clear_screen, process_ok
 from .utils import border
 from python_script_manager.package import PSMReader
+
 
 @click.group(cls=MultiCommand)
 def startproject():
     pass
 
+
 @startproject.command("startproject")
 def startproject_command():
-    # Installing PSM
-    command_process_step("Installing PSM...", "pip install --upgrade python-script-manager", "Installed PSM.")
-    
-    border()
-    # Create psm.json
-    command_process_step("Initializing PSM...",'psm init --template="blank"',"Initialized PSM.")
+    processes = []
+    process_ok(processes)
 
-    border()
+    # Install PSM
+    command_process_step(
+        "Installing PSM...", "pip install --upgrade python-script-manager")
+    processes.append("Installed PSM")
+    process_ok(processes)
+
+    # Create psm.json
+    command_process_step("Initializing PSM...",
+                         'psm init --template="blank"')
+    processes.append("Initialized PSM")
+    process_ok(processes)
+    
     # Create config field
     psm = PSMReader('psm.json')
     psm_config = psm.get_config()
--- flaskmng/utils.py
+++ flaskmng/utils.py
@@ -1,5 +1,8 @@
 import click
 import os
+from sys import platform
+import time
+import pyfiglet
 
 class MultiCommand(click.Group):
     def command(self, *args, **kwargs):
@@ -20,8 +23,21 @@ class MultiCommand(click.Group):
             return cmd
         return decorator
 
-def command_process_step(start_text, command, finish_text):
-    print(start_text)
-    print(f">> {command}")
+
+def command_process_step(start_text, command):
+    print("⌛ "+start_text+"\n")
+    print(f">> {command}\n")
     os.system(command)
-    print(finish_text)
+
+def clear_screen():
+    if platform == "linux" or platform == "linux2" or platform == "darwin":
+        os.system("clear")
+    elif platform == "win32":
+        os.system("cls")
+
+def process_ok(finished_process):
+    clear_screen()
+    text = pyfiglet.figlet_format("flaskmng", font = "slant"  )
+    print(text)
+    for i in finished_process:
+        print("✔ "+i)
